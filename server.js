aa1fc92c3b3c015dafc58d996989290a1943b100
Photon <syamprasad@Photons-MacBook-Pro.local>
2018-08-01 15:42:18 -0400

    changed welcome text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res, next) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hey! this is a test project :)');
 });
 
 app.listen(process.env.PORT || 3000);
